cfc97754d58bbd824e471feba26a1ad4d011feee
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-02-12 16:48:23 +0300

    bir başka shop

---
+++ src/app/data.js
@@ -0,0 +1,19 @@
+module.exports=function(){
+    return {
+        products:[
+            {id:1,name:"Samsung P5",price:1200.52,imageUrl:"1.jpg",description:"fena değil"},
+            {id:2,name:"Samsung P6",price:1200.52,imageUrl:"2.jpg",description:"fena değil"},
+            {id:3,name:"Samsung P7",price:1200.52,imageUrl:"3.jpg",description:"fena değil"},
+            {id:4,name:"Samsung P8",price:1200.52,imageUrl:"4.jpg",description:"fena değil"},
+            {id:5,name:"Samsung P9",price:1200.52,imageUrl:"5.jpg",description:"fena değil"},
+            {id:6,name:"Samsung P10",price:1200.52,imageUrl:"6.jpg",description:"fena değil"},
+            {id:7,name:"Samsung P11",price:1200.52,imageUrl:"7.jpg",description:"fena değil"}
+        ],
+        categories:[
+            {id:1,name:"Telefon"},
+            {id:2,name:"Bilgisayar"},
+            {id:3,name:"Elektronik"}
+        ],
+        orders:[]
+    }
+}
