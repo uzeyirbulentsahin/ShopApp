7aa21bc8df85a8558404c8b2e493169d0227f8d8
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-02-24 10:07:17 +0300

    ürünler listelendi

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -6,7 +6,10 @@ import { ProductReposity } from "../model/product.repository";
 
 @Component({
     selector:'shop',
-    templateUrl:'shop.component.html'
+    templateUrl:'shop.component.html',
+    styles:[`
+        .pt-100 { padding-top:100px; }
+    `]
 })
 
 export class ShopComponent{
