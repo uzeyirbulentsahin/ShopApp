285f8c3d49d46357db6fa1b49764612b4772d074
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-02-24 16:44:47 +0300

    sayfalama

--- data.js
+++ data.js
@@ -4,9 +4,9 @@ module.exports=function(){
             {id:1,name:"Samsung P5",price:1200.52,imageUrl:"1.jpg",description:"fena değil",category:"Telefon"},
             {id:2,name:"Samsung P6",price:1200.52,imageUrl:"2.jpg",description:"fena değil",category:"Telefon"},
             {id:3,name:"Samsung P7",price:1200.52,imageUrl:"3.jpg",description:"fena değil",category:"Telefon"},
-            {id:4,name:"Samsung P8",price:1200.52,imageUrl:"4.jpg",description:"fena değil",category:"Telefon"},
-            {id:5,name:"Samsung P9",price:1200.52,imageUrl:"5.jpg",description:"fena değil",category:"Telefon"},
-            {id:6,name:"Samsung P10",price:1200.52,imageUrl:"6.jpg",description:"fena değil",category:"Telefon"}            
+            {id:4,name:"Samsung P8",price:1200.52,imageUrl:"4.jpg",description:"fena değil",category:"Elektronik"},
+            {id:5,name:"Samsung P9",price:1200.52,imageUrl:"5.jpg",description:"fena değil",category:"Bilgisayar"},
+            {id:6,name:"Samsung P10",price:1200.52,imageUrl:"6.jpg",description:"fena değil",category:"Bilgisayar"}            
         ],
         categories:[
             {id:1,name:"Telefon"},
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,31 +1,47 @@
-import { Component} from "@angular/core";
+import { Component } from "@angular/core";
 import { Category } from "../model/category.model";
 import { CategoryReposity } from "../model/category.repository";
 import { Product } from "../model/product.model";
 import { ProductReposity } from "../model/product.repository";
 
 @Component({
-    selector:'shop',
-    templateUrl:'shop.component.html',
-    styles:[`
+    selector: 'shop',
+    templateUrl: 'shop.component.html',
+    styles: [`
         .pt-100 { padding-top:100px; }
     `]
 })
 
-export class ShopComponent{
-    public selectedCategory:Category=null;
+export class ShopComponent {
+    public selectedCategory: Category = null;
+    public selectedPage = 1;
+    public productPerPage = 4;
 
-    constructor(private categoryRepository:CategoryReposity
-        ,private productRepository:ProductReposity){}
 
-    get products(): Product[]{
-        return this.productRepository.getProducts(this.selectedCategory);
+    constructor(private categoryRepository: CategoryReposity
+        , private productRepository: ProductReposity) { }
+
+    get products(): Product[] {
+        let index = (this.selectedPage - 1) * this.productPerPage
+        return this.productRepository
+            .getProducts(this.selectedCategory)
+            .slice(index, index + this.productPerPage);
     }
-    get categories(): Category[]{
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.productRepository
+            .getProducts(this.selectedCategory).length / this.productPerPage))
+            .fill(0)
+            .map((a, i) => i + 1)
+
+    }
+    get categories(): Category[] {
         return this.categoryRepository.getCategories();
     }
 
-    changeCategory(newCategory?:Category){
-            this.selectedCategory=newCategory;
+    changeCategory(newCategory?: Category) {
+        this.selectedCategory = newCategory;
+    }
+    changePage(p: number) {
+        this.selectedPage = p;
     }
 }
